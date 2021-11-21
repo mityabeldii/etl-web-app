981f5eb4fe08112c242b4a6740fe2c93a678d720
mityabeldii <mityabeldii@gmail.com>
2021-11-21 12:14:33 +0300

    21.11.2021🌿12:14

--- src/api/processes-api.js
+++ src/api/processes-api.js
@@ -117,7 +117,8 @@ const ProcessesAPI = {
             try {
                 const response = (await axios.post(`${base_url}/process/${processId}/process-runs`)).data;
                 await ProcessesAPI.getProcessTasks(processId);
-                handleSuccess([response]);
+                // handleSuccess([response]);
+                handleSuccess({ message: `Процесс с id ${processId} запущен` });
                 return response;
             } catch (error) {
                 throw handleError(error);
