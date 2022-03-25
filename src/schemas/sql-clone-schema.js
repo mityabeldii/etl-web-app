8c51e8937fcd9c129e612ac1f79521ff98503b44
mityabeldii <mityabeldii@gmail.com>
2022-03-25 08:15:55 +0200

    SQL clone schema update

--- src/schemas/sql-clone-schema.js
+++ src/schemas/sql-clone-schema.js
@@ -1,68 +1,66 @@
 /*eslint-disable*/
-const sqlCloneSchema = (yup, data) => ({
-    operatorConfigData: yup
-        .object()
-        .transform((d) => d ?? {})
-        .shape({
-            source: yup
-                .object()
-                .default({})
-                .shape({
-                    sourceId: yup.string().required(`Это поле обязательно`),
-                    sourceSchemaName: yup.string().required(`Это поле обязательно`),
-                    sourceTableName: yup.string().required(`Это поле обязательно`),
-                    sourceTableFields: yup
-                        .array()
-                        .min(1, `Это поле обязательно`)
-                        .of(
-                            yup.object().shape({
-                                sourceFieldName: yup.string().required(`Это поле обязательно`),
-                            })
-                        )
-                        .required(`Это поле обязательно`),
-                }),
-            target: yup
-                .object()
-                .default({})
-                .shape({
-                    targetId: yup.string().required(`Это поле обязательно`),
-                    targetId: yup.string().required(`Это поле обязательно`),
-                    targetSchemaName: yup.string().required(`Это поле обязательно`),
-                    targetTableName: yup.string().required(`Это поле обязательно`),
-                }),
-            mappingStucture: yup
-                .array()
-                .default([])
-                .of(
-                    yup.object().shape({
-                        sourceFieldName: yup.string().required(`Это поле обязательно`),
-                        targetFieldName: yup.string().required(`Это поле обязательно`),
-                    })
-                )
-                .required(`Это поле обязательно`),
-            updateSettings: yup
-                .object()
-                .default({})
-                .shape({
-                    updateType: yup.string().required(`Это поле обязательно`),
-                    ...({
-                        REPLACE: {},
-                        INCREMENT_UPSERT: {
-                            lastUpdatedFieldSource: yup.string().required(`Это поле обязательно`),
-                            lastUpdatedFieldTarget: yup.string().required(`Это поле обязательно`),
-                            primaryKey: yup.string().required(`Это поле обязательно`),
-                        },
-                        INCREMENT_INSERT: {
-                            lastCreatedFieldSource: yup.string().required(`Это поле обязательно`),
-                            lastCreatedFieldTarget: yup.string().required(`Это поле обязательно`),
-                        },
-                        INCREMENT_LOAD: {
-                            lastUpdatedFieldSource: yup.string().required(`Это поле обязательно`),
-                            lastUpdatedFieldTarget: yup.string().required(`Это поле обязательно`),
-                        },
-                    }?.[data?.operatorConfigData?.updateSettings?.updateType] ?? {}),
-                }),
-        }),
-});
+const sqlCloneSchema = (yup, data) => {
+    return {
+        source: yup
+            .object()
+            .default({})
+            .shape({
+                sourceId: yup.string().required(`Это поле обязательно`),
+                sourceSchemaName: yup.string().required(`Это поле обязательно`),
+                sourceTableName: yup.string().required(`Это поле обязательно`),
+                sourceTableFields: yup
+                    .array()
+                    .min(1, `Это поле обязательно`)
+                    .of(
+                        yup.object().shape({
+                            sourceFieldName: yup.string().required(`Это поле обязательно`),
+                        })
+                    )
+                    .required(`Это поле обязательно`),
+            }),
+        target: yup
+            .object()
+            .default({})
+            .shape({
+                targetId: yup.string().required(`Это поле обязательно`),
+                targetId: yup.string().required(`Это поле обязательно`),
+                targetSchemaName: yup.string().required(`Это поле обязательно`),
+                targetTableName: yup.string().required(`Это поле обязательно`),
+            }),
+        mappingStucture: yup
+            .array()
+            .default([])
+            .of(
+                yup.object().shape({
+                    sourceFieldName: yup.string().required(`Это поле обязательно`),
+                    targetFieldName: yup.string().required(`Это поле обязательно`),
+                })
+            )
+            .required(`Это поле обязательно`),
+        updateSettings: yup
+            .object()
+            .default({})
+            .shape({
+                updateType: yup.string().required(`Это поле обязательно`),
+                ...({
+                    REPLACE: {},
+                    INCREMENT_UPSERT: {
+                        lastUpdatedFieldSource: yup.string().required(`Это поле обязательно`),
+                        lastUpdatedFieldTarget: yup.string().required(`Это поле обязательно`),
+                        primaryKey: yup.string().required(`Это поле обязательно`),
+                    },
+                    INCREMENT_INSERT: {
+                        lastCreatedFieldSource: yup.string().required(`Это поле обязательно`),
+                        lastCreatedFieldTarget: yup.string().required(`Это поле обязательно`),
+                    },
+                    INCREMENT_LOAD: {
+                        lastUpdatedFieldSource: yup.string().required(`Это поле обязательно`),
+                        lastUpdatedFieldTarget: yup.string().required(`Это поле обязательно`),
+                    },
+                }?.[data?.operatorConfigData?.updateSettings?.updateType] ?? {}),
+            }),
+    };
+};
+
 export default sqlCloneSchema;
 /*eslint-enable*/
